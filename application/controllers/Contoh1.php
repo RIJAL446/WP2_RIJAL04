9aceff94d2e45836c954782cfaee71eba95b37a6
RIJAL <rizalwarawiri0@gmail.com>
2023-03-20 12:25:20 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends Ci_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rijal, 
+            saya tinggal di daerah Jakarta 
+            olahraga yang saya sukai adalah Basketball";
+    }
+}
