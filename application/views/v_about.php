7a74e2b28e787ee4482ce6f669bf5e1dbd26afc2
RIJAL <rizalwarawiri0@gmail.com>
2023-03-27 16:46:13 +0700

    WP2_RIJAL04

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>RIJAL</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rezael</li>
+        <li>Nama Belakang : Ramli</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Kampung Goestea no.1945 wijaya kusuma</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+        <li>Basket</li>
+    </ul>
+</section>
