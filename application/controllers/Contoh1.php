8d83dfa9e4f5218b90ddce3d15782a6bd94c0351
lestari1521 <lestari1521.siregarlestari31@gmail.com>
2023-07-01 22:31:42 +0700

    WP2_lestari

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends Ci_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Rijal, 
+        echo"Nama saya Lestari siregar, 
             saya tinggal di daerah Jakarta 
             olahraga yang saya sukai adalah Basketball";
     }
